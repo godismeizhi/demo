5cfc4682954d5785803a87dc57e524579812737b
godsimeizhi <godsimeizhi@mail.com>
2019-12-19 22:43:18 +0800

    修改

---
+++ src/main/java/com/example/demo/excel/ExcelToJson.java
@@ -0,0 +1,50 @@
+package com.example.demo.excel;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExcelToJson {
+
+    public static void main(String[] args) throws Exception {
+
+        Map<String, List<List<String>>> excelData = ExcelUtil.readExcel("E:\\test.xlsx");
+
+
+        List<List<String>> infos = excelData.get("Sheet1");
+        List<String> infoTitle = infos.get(0);
+        infos.remove(0);
+
+        List<Map<String, Object>> outputData = new ArrayList<>();
+
+        for (List<String> infoLine : infos) {
+
+            Map<String, Object> data = new HashMap<>();
+            for (int i = 0; i < infoLine.size(); i++) {
+                data.put(infoTitle.get(i), infoLine.get(i));
+            }
+            outputData.add(data);
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = objectMapper.writeValueAsString(outputData);
+
+        System.out.println("==========" + result);
+
+        FileWriter writer;
+        try {
+            writer = new FileWriter("E:/result.json");
+            writer.write(result);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
